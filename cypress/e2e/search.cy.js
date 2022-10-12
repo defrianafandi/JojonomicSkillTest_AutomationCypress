c6e356d3e2d72c061134e9591e31386730b45f75
defrianafandi <defrianfandi@refactory.id>
2022-10-12 22:50:42 +0700

    test5

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -6,15 +6,16 @@ let base_url = Cypress.env('base_url')
 describe('Test search and find locations', () => {
   it('Case 1: sucessfully search with valid data', () => {
     cy.visit(`${base_url}`)
+    cy.viewport(1400, 800)
     Cypress.on('uncaught:exception', (err, runnable) => {
       return false
     })
-    cy.get('#homeautosuggestpTPTHOb9F9A > .form__text').type(search_valid.toString(),{force: true, delay: 0})
+    cy.get('[name="q"]').eq(2).type(search_valid.toString(),{force: true, delay: 0})
     cy.contains(search_valid.toLowerCase()).should('be.visible')
-    cy.get('#homeautosuggestpTPTHOb9F9A > .form__text').type('{enter}',{force: true, delay: 0})
+    cy.get('[name="q"]').eq(2).type('{enter}',{force: true, delay: 0})
     cy.wait(1000)
     cy.contains('Apply for a number plate').should('be.visible')
-    cy.get('.search__result').eq(2).scrollIntoView().should('be.visible')
+    cy.get('.search__result').eq(1).scrollIntoView().should('be.visible')
     cy.contains('number').should('be.visible')  
   })
 

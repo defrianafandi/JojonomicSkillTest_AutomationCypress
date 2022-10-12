2e39c3f5008aeb10fb0b6e474c1202f9c4f67a09
defrianafandi <defrianfandi@refactory.id>
2022-10-12 22:25:28 +0700

    test4

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -6,12 +6,13 @@ let base_url = Cypress.env('base_url')
 describe('Test search and find locations', () => {
   it('Case 1: sucessfully search with valid data', () => {
     cy.visit(`${base_url}`)
-    cy.viewport(1500, 800)
     Cypress.on('uncaught:exception', (err, runnable) => {
       return false
     })
-    cy.get('#homeautosuggestpTPTHOb9F9A > .form__text').type(search_valid.toString())
+    cy.get('#homeautosuggestpTPTHOb9F9A > .form__text').type(search_valid.toString(),{force: true, delay: 0})
+    cy.contains(search_valid.toLowerCase()).should('be.visible')
     cy.get('#homeautosuggestpTPTHOb9F9A > .form__text').type('{enter}',{force: true, delay: 0})
+    cy.wait(1000)
     cy.contains('Apply for a number plate').should('be.visible')
     cy.get('.search__result').eq(2).scrollIntoView().should('be.visible')
     cy.contains('number').should('be.visible')  
@@ -21,7 +22,8 @@ describe('Test search and find locations', () => {
     cy.visit(`${base_url}`)
     cy.viewport(1500, 800)
     cy.contains('Find locations').click()
-    cy.get('#locatorTextSearch').focus().clear().type(loc_valid.toString())
+    cy.get('#locatorTextSearch').focus().clear().type(loc_valid.toString(),{force: true, delay: 0})
+    cy.wait(1000)
     cy.get('#locatorTextSearch').type('{enter}',{force: true, delay: 0})
     cy.contains('Rozelle Bay Maritime Service Centre').should('be.visible')
   })

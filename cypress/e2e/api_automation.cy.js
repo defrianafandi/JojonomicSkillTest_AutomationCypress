ef4dae2f4b33b7fb48eb06932a2205ba6a9512ed
defrianafandi <defrianafandi@gmail.com>
2022-10-14 13:54:31 +0700

    Fix automation

--- cypress/e2e/api_automation.cy.js
+++ cypress/e2e/api_automation.cy.js
@@ -1,16 +1,15 @@
-let lat = 17
-let lon = 71
-let result_state_code = 16
-let postal_code = 99501
+const base_URL = Cypress.env('url_api')
+Cypress.config('baseUrl', `${base_URL}`)
+const token_key = Cypress.env('api_key')
+const lat = 17
+const lon = 71
+const result_state_code = 16
+const postal_code = 99501
 
 describe('API Testing', () => {
-    let base_URL = Cypress.env('url_api')
-    Cypress.config('baseUrl', `${base_URL}`)
-    let token_key = Cypress.env('api_key')
-
     it('Case 1: Get Current Weather Data where given a lat/lon', () => {
         cy.request(`/current?lat=${lat}&lon=${lon}&key=${token_key}`).then((response) => {
-            let jsonData = response.body.data[0].state_code
+            const jsonData = response.body.data[0].state_code
             cy.log(jsonData)
 
             expect(response).to.have.property('status', 200)
@@ -21,9 +20,9 @@ describe('API Testing', () => {
 
     it('Case 2: Get Current Weather Data where given a postal code', () => {
         cy.request(`/current?postal_code=${postal_code}&key=${token_key}`).then((response) => {
-            let dataTimeStampUTC = response.body.data[0].ob_time
-            let dataWeather = response.body.data[0].weather
-            let jsonData = JSON.stringify(dataTimeStampUTC,dataWeather)
+            const dataTimeStampUTC = response.body.data[0].ob_time
+            const dataWeather = response.body.data[0].weather
+            const jsonData = JSON.stringify(dataTimeStampUTC,dataWeather)
             cy.log(jsonData)
 
             expect(response).to.have.property('status', 200)
